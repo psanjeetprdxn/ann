2f6e9ce90951fb4fbd6faecb652c2d286761a315
sanjeet <sanjeetp@prdxn.com>
2019-01-21 19:49:34 +0530

    Slider script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,10 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  $('.slides').slick({
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 1,
+    nextArrow:$('.next'),
+    prevArrow:$('.prev')
+  });
+});
