cdd0cc408cd339fc74c25c580193129c8ca198da
sanjeet <sanjeetp@prdxn.com>
2019-01-22 11:38:31 +0530

    tabs-filter added

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,35 @@ $(document).ready(function(){
     prevArrow:$('.prev')
   });
 });
+
+
+$(document).ready(function() {
+  // Initially showing social class <div> and hiding rest of them.
+  $('#ourHolder').children('div.social').show();
+  $('#ourHolder').children('div:not(.social)').hide();
+
+	$('#filterOptions li span').click(function() {
+		// fetch the class of the clicked item
+		let ourClass = $(this).attr('class');
+
+		// reset the active class on all the buttons
+		$('#filterOptions li').removeClass('active');
+		// update the active state on our clicked button
+		$(this).parent().addClass('active');
+
+		if(ourClass == 'social') {
+			// show all our items
+			$('#ourHolder').children('div.social').show();
+
+      // hide all elements that don't share ourClass
+			$('#ourHolder').children('div:not(.' + ourClass + ')').hide();
+    }
+		else {
+			// hide all elements that don't share ourClass
+			$('#ourHolder').children('div:not(.' + ourClass + ')').hide();
+			// show all elements that do share ourClass
+			$('#ourHolder').children('div.' + ourClass).show();
+		}
+		return false;
+	});
+});
